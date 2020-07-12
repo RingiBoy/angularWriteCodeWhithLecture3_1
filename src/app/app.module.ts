783a1c090f1692f1e0212f4fd66feb052bb6fd35
ringi <mitya.info@gmail.com>
2020-07-12 21:41:02 +0300

    activatedRoute.params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,36 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './components/app.component';
 import {HttpClientModule} from '@angular/common/http';
 
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {AllUsersComponent} from './components/all-users/all-users.component';
 import { UserComponent } from './components/user/user.component';
+import { AllPostsComponent } from './components/all-posts/all-posts.component';
+import { PostComponent } from './components/post/post.component';
+import { AllCommentsComponent } from './components/all-comments/all-comments.component';
+import { CommentComponent } from './components/comment/comment.component';
+
+const links: Routes = [
+  {path: 'viewItRoutForShowAllUsers', component: AllUsersComponent},
+  {path: 'viewItRoutForShowAllPosts', component: AllPostsComponent},
+  {path: 'viewItRoutForShowAllComments', component: AllCommentsComponent},
+  {path: 'posts/:id', component: AllPostsComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AllUsersComponent,
     UserComponent,
+    AllPostsComponent,
+    PostComponent,
+    AllCommentsComponent,
+    CommentComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'viewItRoutForShowAllUsers', component: AllUsersComponent},
-      // {path: 'viewItRoutForShowAllPosts'}
-    ]),
+    RouterModule.forRoot(links),
   ],
   providers: [],
   bootstrap: [AppComponent]
